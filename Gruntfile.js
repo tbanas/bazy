fbebe1d7330536730bed577e616bbf129e08aede
Tomasz Banaś <tomek.kacper@gmail.com>
2016-06-20 16:58:14 +0200

    Installed bower & grunt

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        bowercopy: {
+            options: {
+                srcPrefix: 'bower_components',
+                destPrefix: 'web/assets'
+            },
+            scripts: {
+                files: {
+                    'js/jquery.js': 'jquery/dist/jquery.js',
+                    'js/bootstrap.js': 'bootstrap/dist/js/bootstrap.js'
+                }
+            },
+            stylesheets: {
+                files: {
+                    'css/bootstrap.css': 'bootstrap/dist/css/bootstrap.css',
+                }
+            },
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-bowercopy');
+    grunt.registerTask('default', ['bowercopy']);
+};
